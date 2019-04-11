eb76814db6a1e6acaf4b28838d3eacb0da22418f
JsyTim <34913869+JsyTim@users.noreply.github.com>
2019-04-11 15:19:37 -0500

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -4,69 +4,56 @@ var map = L.map('geospatial', {
   scrollWheelZoom: false,
   dragging: false,
   attributionControl: false,
-}).setView([0.074329, 30.534796], 1.75);
+}).setView([10.074329, 42.534796], 2);
 
-//initialize an info control
-var info = L.control();
-
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
-
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-
-    this._div.innerHTML = '<h4>Detailed Information</h4>' +
-        //ternary
-        (props ?
-        //if hovering on a district
-          "District: " + props.Region +
-          "<br>Water:" + props.Water + "%" +
-          "<br>Help: " + props.Help +
-          "<br>Food: " + props.Food + "%"
-        //if not hovering on a district
-        : 'Hover over a district');
-};
-
-//add info to map
-info.addTo(map);
+// //initialize an info control
+// var info = L.control();
+//
+// info.onAdd = function (map) {
+//     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+//     this.update();
+//     return this._div;
+// };
+//
+// // method that we will use to update the control based on feature properties passed
+// info.update = function (props) {
+//
+//     this._div.innerHTML = '<h4>Detailed Information</h4>' +
+//         //ternary
+//         (props ?
+//         //if hovering on a district
+//           "District: " + props.Region +
+//           "<br>Water:" + props.Water + "%" +
+//           "<br>Help: " + props.Help +
+//           "<br>Food: " + props.Food + "%"
+//         //if not hovering on a district
+//         : 'Hover over a district');
+// };
+//
+// //add info to map
+// info.addTo(map);
 
 //merge the two data sets
-for (i = 0; i < districts.features.length; i++) {
-  districts.features[i].properties.dist_num = i
-  // for (j = 0; j < region_concern.features.length; j++) {
-  //   if (districts.features[i].properties.dist_num == region_concern.features[j].region) {
-  //       districts.features[i].properties.count = region_concern.features[j].;
-  //   }
-  // }
-};
+// for (i = 0; i < districts.features.length; i++) {
+//   districts.features[i].properties.dist_num = i
+//   // for (j = 0; j < region_concern.features.length; j++) {
+//   //   if (districts.features[i].properties.dist_num == region_concern.features[j].region) {
+//   //       districts.features[i].properties.count = region_concern.features[j].;
+//   //   }
+//   // }
+// };
+year = '1995'
 for (i = 0; i < districts.features.length; i++) {
   // districts.features[i].properties.dist_num = i
-  for (j = 0; j < region_concern.features.length; j++) {
-    if (districts.features[i].properties.dist_num == region_concern.features[j].dist_num) {
-        districts.features[i].properties.count = region_concern.features[j].count;
+  districts.features[i].properties.genres_nums = []
+  for (j = 0; j < region_concern[year].length; j++) {
+    if (districts.features[i].id == region_concern[year][j]['region_name'] || districts.features[i].properties.name == region_concern[year][j]['region_name']) {
+        districts.features[i].properties.count = region_concern[year][j]['counts'];
+        districts.features[i].properties.genres_nums = region_concern[year][j]['genres_nums'];
     }
   }
 };
-console.log(districts);
-//merge the two data sets
-for (i=0; i<districts.features.length; i++) {
-    //console.log("districts.features[i].properties ", typeof districts.features[i].properties);
-    //console.log("districts.features.properties.dist_num ", districts.features[i].properties.dist_num);
-    // for (j=0; j<test_data.features.length; j++) {
-        // if (districts.features[i].properties.dist_num == test_data.features[j].dist_num) {
-        //     //console.log("IN DISTRICTS", districts.features[i].properties.dist_num, "IN TEST DATA", test_data.features[j].dist_num);
-        //     districts.features[i].properties.Region = test_data.features[j].Region;
-        //     districts.features[i].properties.Water = test_data.features[j].Water;
-        //     districts.features[i].properties.Help = test_data.features[j].Help;
-        //     districts.features[i].properties.Food = test_data.features[j].Food;
-        // }
-    }
-    //districts.features[i].properties.anotherItem = "Whatever";
-    //console.log('districts.features.properties.anotherItem', districts.features[i].properties.anotherItem);
-// };
+console.log(region_concern['1995']);
 
 var geojson;
 
@@ -138,11 +125,107 @@ function style(feature, color, item) {
     return merge_styles(default_style, style_override);
 };
 
+function genreBar(nums){
+    var categories= ['Action', 'Adventure', 'Animation', 'Children', 'Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','IMAX','Musical','Mystery','Romance','Sci-Fi', 'Thriller', 'War'];
+
+    var colors = ["#F78571","#F98286","#F4839C","#E788B1","#D490C2","#BB99CF","#9DA2D6","#7DAAD7","#5BB1D0","#3CB6C4","#2CB9B3","#37BA9E","#4EBA87","#68B871","#82B55D","#9BB04E","#B3AA44","#C9A243","#DD9A4A","#ED9157"];
+
+		var grid = d3.range(25).map(function(i){
+			return {'x1':0,'y1':0,'x2':0,'y2':480};
+		});
+
+		var tickVals = grid.map(function(d,i){
+			if(i>0){ return i*10; }
+			else if(i===0){ return "100";}
+		});
+
+		var xscale = d3.scaleLinear()
+						.domain([0,60])
+						.range([0,700]);
+
+		var yscale = d3.scaleLinear()
+						.domain([0,categories.length])
+						.range([0,480]);
+
+		var colorScale = d3.scaleQuantize()
+						.domain([0,categories.length])
+						.range(colors);
+
+		var canvas = d3.select('#wrapper')
+						.append('svg')
+						.attr('width',900)
+						.attr('height',550)
+
+		// var grids = canvas.append('g')
+		// 				  .attr('id','grid')
+		// 				  .attr('transform','translate(150,10)')
+		// 				  .selectAll('line')
+		// 				  .data(grid)
+		// 				  .enter()
+		// 				  .append('line')
+		// 				  .attr('x1', function(d,i){ return i*30;})
+		// 				  .attr('y1',function(d){ return d.y1; })
+		// 				  .attr('x2',function(d,i){ return i*30; })
+		// 				  .attr('y2',function(d){ return d.y2; })
+		// 					.style('stroke','#adadad')
+		// 					.style('stroke-width','1px');
+
+		var xAxis = d3.axisBottom(xscale).tickValues(tickVals);
+		var yAxis = d3.axisLeft(yscale)
+					.tickSize(2)
+					.tickFormat(function(d,i){ return categories[i]; })
+					.tickValues(d3.range(17));
+
+		var y_xis = canvas.append('g')
+						  .attr("transform", "translate(150,0)")
+						  .attr('id','yaxis')
+						  .call(yAxis);
+
+		var x_xis = canvas.append('g')
+						  .attr("transform", "translate(150,480)")
+						  .attr('id','xaxis')
+						  .call(xAxis);
+
+		var chart = canvas.append('g')
+							.attr("transform", "translate(150,0)")
+							.attr('id','bars')
+							.selectAll('rect')
+							.data(nums)
+							.enter()
+							.append('rect')
+							.attr('height',19)
+							.attr('x',0)
+							.attr('y',function(d,i){ return yscale(i)-10; })
+							.style('fill',function(d,i){ return colorScale(i); })
+							.attr('width',function(d){ return 0; });
+
+
+		var transit = d3.select("svg").selectAll("rect")
+						    .data(nums)
+						    .transition()
+						    .duration(1000)
+						    .attr("width", function(d) {return xscale(d); });
+
+		var transitext = d3.select('#bars')
+							.selectAll('text')
+							.data(nums)
+							.enter()
+							.append('text')
+							.attr('x', function(d) {return xscale(d)-200; })
+							.attr('y', function(d,i){ return yscale(i)+35; })
+							.text(function(d){ return d; }).style({'fill':'#fff','font-size':'14px'});
+}
+
 var geojson = L.geoJson(districts, {
     style: style,
     onEachFeature: onEachFeature
 }).addTo(map);
 // L.geoJson(districts).addTo(map);
+function clean() {
+  $("#wrapper").empty();
+  // var html = '<g><text></text></g>'
+  // $("#chart").append(html);
+}
 
 function highlightFeature(e) {
     var layer = e.target;
@@ -158,9 +241,13 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera) {
         layer.bringToFront();
     }
-
+    clean();
+    var nums = []
+    nums = layer.feature.properties.genres_nums;
+    console.log(nums)
+    genreBar(nums);
      //on hover change infobox
-    info.update(layer.feature.properties);
+    // info.update(layer.feature.properties);
 }
 
 
@@ -168,7 +255,7 @@ function highlightFeature(e) {
 function resetHighlight(e) {
     style_override = {};
     geojson.resetStyle(e.target);
-    info.update();
+    // info.update();
 }
 
 
@@ -206,116 +293,23 @@ legend.onAdd = function (map) {
 };
 legend.addTo(map);
 
-//
-// //click to change color style to Water categories
-// document.getElementById("change-water")
-//         .addEventListener('click', function () {
-//           style_target = function(f) { return f.properties.Water };
-//           style_item = function(f){ return 'water' };
-//           geojson.setStyle(style);
-//
-//           if (legend instanceof L.Control) { map.removeControl(legend); }
-//
-//           legend = L.control({position: 'topright'});
-//
-//           legend.onAdd = function (map) {
-//
-//             var div = L.DomUtil.create('div', 'info legend'),
-//                 grades = ['0', '20','40','60','80','100'],
-//                 labels = [];
-//
-//             // loop through categories and generate a label with a colored square for each interval
-//             for (var i = 0; i < grades.length; i++) {
-//                 div.innerHTML +=
-//                     '<i style="background:' + getColor(grades[i], 'water') + '"></i> ' +
-//                     grades[i]  + '<br>' ;
-//             }
-//             console.log("DIV FOR CHANGE WATER: ", div.innerHTML);
-//             div.innerHTML += "<i style='background:gray'></i> unknown<br>"
-//             return div;
-//           };
-//           legend.addTo(map);
-//        });
-//
-// //click to change color style to Help categories
-// document.getElementById("change-help")
-//         .addEventListener('click', function () {
-//           style_target = function(f) { return f.properties.Help };
-//           style_item = function(f){ return 'help' };
-//           geojson.setStyle(style);
-//
-//           if (legend instanceof L.Control) { map.removeControl(legend); }
-//
-//           legend = L.control({position: 'topright'});
-//
-//           legend.onAdd = function (map) {
-//
-//             var div = L.DomUtil.create('div', 'info legend'),
-//                 grades = ['10','20','50','100','200','500','1000'],
-//                 labels = [];
-//
-//             // loop through categories and generate a label with a colored square for each interval
-//             for (var i = 0; i < grades.length; i++) {
-//                 div.innerHTML +=
-//                     '<i style="background:' + getColor(grades[i], 'help') + '"></i> ' +
-//                     grades[i]  + '<br>' ;
-//             }
-//             div.innerHTML += "<i style='background:gray'></i> unknown<br>"
-//
-//             return div;
-//           };
-//            legend.addTo(map);
-//        });
-//
-//
-//
-// //click to change color style to Food categories
-// document.getElementById("change-food")
-//         .addEventListener('click', function () {
-//           style_target = function(f) { return f.properties.Food };
-//           style_item = function(f){ return 'food' };
-//           geojson.setStyle(style);
-//
-//           if (legend instanceof L.Control) { map.removeControl(legend); }
-//
-//           legend = L.control({position: 'topright'});
-//
-//           legend.onAdd = function (map) {
-//
-//             var div = L.DomUtil.create('div', 'info legend'),
-//                 grades = ['0', '20','40','60','80','100'],
-//                 labels = [];
-//
-//             // loop through categories and generate a label with a colored square for each interval
-//             for (var i = 0; i < grades.length; i++) {
-//                 div.innerHTML +=
-//                     '<i style="background:' + getColor(grades[i], 'food') + '"></i> ' +
-//                     grades[i]  + '<br>' ;
-//             }
-//             div.innerHTML += "<i style='background:gray'></i> unknown<br>"
-//
-//             return div;
-//           };
-//            legend.addTo(map);
-//        });
-/*
-//click to change color style to race categories
-document.getElementById("plain-style")
-        .addEventListener('click', function () {
-          style_target = function(f) { return f.properties.genderx };
-          geojson.setStyle(style);
+function countsByYear(year){
+  //merge the two data sets
+  for (i = 0; i < districts.features.length; i++) {
+    // districts.features[i].properties.dist_num = i
+    for (j = 0; j < region_concern[year].length; j++) {
+      if (districts.features[i].id == region_concern[year][j]['region_name'] || districts.features[i].properties.name == region_concern[year][j]['region_name']) {
+          districts.features[i].properties.count = region_concern[year][j]['counts'];
+      }
+    }
+  };
 
-          if (legend instanceof L.Control) { map.removeControl(legend); }
+  map.eachLayer(function (layer) {
+    map.removeLayer(layer)
+  });
 
-          legend = L.control({position: 'topright'});
-
-          legend.onAdd = function (map) {
-
-            //had to create a dummy div to get add/remove to work.
-            var div = L.DomUtil.create('div', 'info legend')//,
-            return div;
-          };
-          legend.addTo(map);
-
-       });
-*/
+  var geojson = L.geoJson(districts, {
+      style: style,
+      onEachFeature: onEachFeature
+  }).addTo(map);
+}
